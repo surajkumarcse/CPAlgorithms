efd8f19ebe00f4970ebd37088c169a5feb68773c
Suraj Kumar <30420005+surajkumarcse@users.noreply.github.com>
2019-01-02 02:01:51 +0530

    Add files via upload
    It is not tested yet, it's just copied from my previously available files in my box.

---
+++ Disjoint_Union.cpp
@@ -0,0 +1,21 @@
+class DisjointUnion {
+    vector<int> id;
+    vector<int> sz;
+    DisjointUnion() {
+        for(int i = 0; i<MAX; i++) {
+            id.push_back(i);
+            sz.push_back(1);
+        }
+    }
+    int getParent(int i) {
+        if(id[i] != i) id[i] = getParent(id[i]);
+        return id[i];
+    }
+    void unionJoin(int x, int y) {
+        x = getParent(x);
+        y = getParent(y);
+        if( x == y ) return;
+        if(sz[x] > sz[y]) { id[y] = x; sz[x] += sz[y]; }
+        else { id[x] = y; sz[y] += sz[x]; }
+    }
+};
