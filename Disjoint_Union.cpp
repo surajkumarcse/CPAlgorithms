adc990127bb5631a7ac70d7cc956677904e08d6a
Suraj Kumar <30420005+surajkumarcse@users.noreply.github.com>
2019-01-02 02:03:54 +0530

    Update Disjoint_Union.cpp

--- Disjoint_Union.cpp
+++ Disjoint_Union.cpp
@@ -1,16 +1,22 @@
 class DisjointUnion {
+    
     vector<int> id;
     vector<int> sz;
+
+    public:
+
     DisjointUnion() {
         for(int i = 0; i<MAX; i++) {
             id.push_back(i);
             sz.push_back(1);
         }
     }
+    
     int getParent(int i) {
         if(id[i] != i) id[i] = getParent(id[i]);
         return id[i];
     }
+    
     void unionJoin(int x, int y) {
         x = getParent(x);
         y = getParent(y);
